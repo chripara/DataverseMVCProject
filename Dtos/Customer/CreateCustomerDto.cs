193c3a10570f154d5e2936ca9b68e3d1212575d7
Christos Paraskevas <45561681+chripara@users.noreply.github.com>
2022-11-28 14:42:35 +0200

    Create and Update CustomerDtos Data annotations added  for Validation

--- Dtos/Customer/CreateCustomerDto.cs
+++ Dtos/Customer/CreateCustomerDto.cs
@@ -1,11 +1,24 @@
-﻿namespace DataverseMVCProject.Dtos.Customer
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataverseMVCProject.Dtos.Customer
 {
     public class CreateCustomerDto
     {
+        [Required]
+        [MaxLength(50)]
         public string Firstname { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Surname { get; set; }
+        [Required]
+        [MaxLength(20)]
         public string ContactPhone { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Address { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
--- Dtos/Customer/UpdateCustomerDto.cs
+++ Dtos/Customer/UpdateCustomerDto.cs
@@ -1,12 +1,31 @@
-﻿namespace DataverseMVCProject.Dtos.Customer
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataverseMVCProject.Dtos.Customer
 {
     public class UpdateCustomerDto
     {
+        [Required]
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
         public string Firstname { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
         public string Surname { get; set; }
+        
+        [Required]
+        [MaxLength(20)]
         public string ContactPhone { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
         public string Address { get; set; }
+        
+        [Required]
+        [MaxLength(30)]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
